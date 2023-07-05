578735977fc7ac7263d81e5df2a195fff8b3dcf0
capitoni92 <138166741+capitoni92@users.noreply.github.com>
2023-07-05 07:11:35 -0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,84 @@
+define(function(require) {
+
+    var Scene = require('scene');
+
+    var basicVert = require('text!shaders/basic.vert');
+    var circleFrag = require('text!shaders/circle.frag');
+    var thresholdFrag = require('text!shaders/threshold.frag');
+    var grayscottFrag = require('text!shaders/grayscott-pmneila.frag');
+
+    var previousPower = function(x) {
+        x = x | (x >> 1);
+        x = x | (x >> 2);
+        x = x | (x >> 4);
+        x = x | (x >> 8);
+        x = x | (x >> 16);
+        return x - (x >> 1);
+    }
+
+    var scene = new Scene(
+        previousPower(document.body.clientWidth),
+        previousPower(document.body.clientHeight)
+    );
+    var originProg = scene.createProgramInfo(basicVert, circleFrag);
+    var thresholdProg = scene.createProgramInfo(basicVert, thresholdFrag);
+    var grayscottProg = scene.createProgramInfo(basicVert, grayscottFrag);
+
+    var scale = 2;
+    var bufferA = scene.createBuffer(
+        scene.width / scale,
+        scene.height / scale
+    );
+    var bufferB = scene.createBuffer(
+        scene.width / scale,
+        scene.height / scale
+    );
+
+    scene.draw({
+        program: originProg,
+        output: bufferA
+    });
+
+    var mLastTime = Number(new Date());
+
+    function render(time) {
+        var dt = (time - mLastTime)/20.0;
+        if(dt > 0.8 || dt<=0)
+            dt = 0.8;
+        mLastTime = time;
+
+        var steps = 8;
+        var lastOutput = bufferA;
+
+        for (var i = 0; i < steps; i++) {
+            var input = lastOutput;
+            var output = (lastOutput === bufferA) ? bufferB : bufferA;
+            lastOutput = output;
+            scene.draw({
+                program: grayscottProg,
+                uniforms: {
+                    delta: dt,
+                    feed: 0.037,
+                    kill: 0.06
+                },
+                inputs: {
+                    tSource: input
+                },
+                output: output
+            });
+        }
+
+        scene.draw({
+            program: thresholdProg,
+            uniforms: {
+                threshold: 0.2
+            },
+            inputs: {
+                u_texture: bufferA
+            }
+        });
+
+        requestAnimationFrame(render);
+    }
+    requestAnimationFrame(render);
+});
---
+++ js/scene.js
@@ -0,0 +1,102 @@
+define(function(require) {
+
+    var twgl = require('lib/twgl');
+
+    twgl.setAttributePrefix("a_");
+
+    var Scene = function(width, height) {
+        var canvas = document.createElement('canvas');
+        document.body.appendChild(canvas);
+
+        this.gl = twgl.getWebGLContext(canvas);
+        this.gl.getExtension( 'OES_texture_float' );
+        this.gl.getExtension( 'OES_texture_float_linear' );
+
+        var arrays = {
+            position: [-1, -1, 0, 1, -1, 0, -1, 1, 0, -1, 1, 0, 1, -1, 0, 1, 1, 0],
+        };
+        this.bufferInfo = twgl.createBufferInfoFromArrays(this.gl, arrays);
+
+        this.fbi = twgl.createFramebufferInfo(this.gl);
+        
+        this.width = width;
+        this.height = height;
+        this.gl.canvas.width = this.width;
+        this.gl.canvas.height = this.height;
+        this.gl.viewport(0, 0, this.width, this.height);
+    }
+
+    Scene.prototype.createBuffer = function(width, height) {
+        width = width || this.width;
+        height = height || this.height;
+        var attachments = [
+            {
+                format: this.gl.RGBA,
+                type: this.gl.FLOAT,
+                min: this.gl.LINEAR,
+                mag: this.gl.LINEAR,
+                wrap: this.gl.REPEAT
+            }
+        ];
+        var fbi = twgl.createFramebufferInfo(
+            this.gl,
+            attachments,
+            width,
+            height
+        )
+        fbi.width = width;
+        fbi.height = height;
+        return fbi;
+    };
+
+    Scene.prototype.createProgramInfo = function(vs, fs) {
+        return twgl.createProgramInfo(this.gl, [vs, fs]);
+    };
+
+    Scene.prototype.drawLastBuffer = function() {
+        this.gl.bindFramebuffer(this.gl.FRAMEBUFFER, null);
+        twgl.drawBufferInfo(this.gl, this.gl.TRIANGLES, this.bufferInfo);
+    }
+
+    Scene.prototype.draw = function(spec) {
+
+        this.gl.useProgram(spec.program.program);
+        
+        var uniforms = {};
+
+        if (spec.uniforms) {
+            Object.assign(uniforms, spec.uniforms);
+        }
+
+        if (spec.inputs) {
+            var inputs = {};
+            Object.keys(spec.inputs).map(function(key, index) {
+                inputs[key] = spec.inputs[key].attachments[0];
+            });
+            Object.assign(uniforms, inputs);
+        }
+
+        var resolution = [this.width, this.height];
+
+        if (spec.output) {
+            var resolution = [spec.output.width, spec.output.height];
+        }
+
+        Object.assign(uniforms, {
+            resolution: resolution,
+        });
+
+        twgl.setUniforms(spec.program, uniforms);
+
+        if (spec.output) {
+            this.gl.bindFramebuffer(this.gl.FRAMEBUFFER, spec.output.framebuffer);
+        } else {
+            this.gl.bindFramebuffer(this.gl.FRAMEBUFFER, null);
+        }
+
+        twgl.setBuffersAndAttributes(this.gl, spec.program, this.bufferInfo);
+        twgl.drawBufferInfo(this.gl, this.gl.TRIANGLES, this.bufferInfo);
+    }
+
+    return Scene;
+});
